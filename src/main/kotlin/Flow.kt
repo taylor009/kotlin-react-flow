ef031f077acf523a793c9935e7d3f19476dbff66
taylor <taylor@affair.dev>
2020-05-12 15:23:46 -0400

    Added props to flow component map and added to Main.kt

--- src/main/kotlin/Flow.kt
+++ src/main/kotlin/Flow.kt
@@ -1,12 +1,13 @@
 @file:JsModule("react-flow-renderer")
+@file:JsNonModule
 
 import react.RClass
 import react.RProps
 
 @JsName("default")
-external val ReactFlow: RClass<dynamic>
+external val ReactFlow: RClass<ReactFlowProps>
 
 external interface ReactFlowProps : RProps {
     var elements: Array<dynamic>
-
+    var styles: Any?
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import org.w3c.dom.css.CSS
 import react.*
 import react.dom.button
 import react.dom.h1
@@ -6,6 +7,14 @@ import styled.css
 import styled.styledDiv
 import kotlin.browser.document
 
+/*
+  { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+ */
+var elementsTo = arrayOf<Map<Any,Any>>(
+    mapOf("1" to "id", "data" to mapOf("label" to  "node1"), "position" to mapOf(50 to "x", 5 to "y"))
+    )
 
 
 fun main() {
@@ -18,8 +27,10 @@ fun main() {
             css {
 
             }
-            button {
-                +"Hello"
+            ReactFlow{
+                attrs {
+                    elements = elementsTo
+                }
             }
         }
     }
