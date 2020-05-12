e423cbf25589a75d83bd6ae6fb8073e2297306ae
taylor <taylor@affair.dev>
2020-05-12 18:59:59 -0400

    react flow graph is visible

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import kotlinx.css.*
 import kotlinx.serialization.json.Json
 import org.w3c.dom.css.CSS
 import react.*
@@ -10,6 +11,7 @@ import kotlin.browser.document
 import kotlin.js.json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
+
 
 /*
   { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
@@ -28,9 +30,7 @@ var elementsTo = arrayOf<Map<Any,Any>>(
     mapOf("id" to "2", "data" to mapOf("label" to "node2"), "position" to mapOf("x" to 100, "y" to 100)),
     mapOf("id" to "e1-2", "source" to "1", "target" to "2", "animated" to true)
     )
-//
-//
-//
+
 //var elementsTo = <Map<Any,Any>>(
 //    mapOf("id" to "1", "data" to mapOf("label" to  "node1"), "position" to mapOf("x" to 50, "y" to 5)),
 //    mapOf("id" to "2", "data" to mapOf("label" to "node2"), "position" to mapOf("x" to 100, "y" to 100)),
@@ -52,8 +52,15 @@ println("n1 $n1")
             }
             ReactFlow {
                 attrs {
-                    elements = arrayOf(MyElem("22", Coords(100, 100), Labels("asdfasdf")))
-
+                    elements = arrayOf(
+                        MyElem("22", Coords(100, 100), Labels("Node 1")),
+                        MyElem("23", Coords(50, 50), Labels("Node 2"))
+                    )
+                    styles = this@styledDiv.css {
+                        height = 500.pt
+                        width = 500.pt
+                        backgroundColor = Color.whiteSmoke
+                    }
                 }
             }
         }
