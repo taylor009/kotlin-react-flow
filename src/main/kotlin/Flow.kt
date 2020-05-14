0d9277820e7b1662dcb03ad2cd794e26c8c3bf47
jp <jp@skibeach.com>
2020-05-13 23:08:25 -0400

    little progress

--- src/main/kotlin/Flow.kt
+++ src/main/kotlin/Flow.kt
@@ -1,9 +1,6 @@
 @file:JsModule("react-flow-renderer")
 @file:JsNonModule
 
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
 import react.RClass
 import react.RProps
 
@@ -11,6 +8,6 @@ import react.RProps
 external val ReactFlow: RClass<ReactFlowProps>
 
 external interface ReactFlowProps : RProps {
-    var elements: Array<MyElem>
+    var elements: Array<ReactFlowElement>
     var styles: Any?
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,31 @@
 import kotlinx.css.*
 import kotlinx.serialization.json.Json
-import org.w3c.dom.css.CSS
-import react.*
-import react.dom.button
 import react.dom.h1
 import react.dom.render
 import styled.css
 import styled.styledDiv
 import kotlin.browser.document
-import kotlin.js.json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonObject
-
-
-/*
-  { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
-  { id: 'e1-2', source: '1', target: '2', animated: true },
- */
 
 val n1 = Json.parseJson("""[{ "id": "1", "position":{"x":100, "y":100} }]""")  //, \"data\": { \"label\": \"Node 1\" }, position: {\"x\": 250, \"y\": 5 }
 
-data class Coords(val x: Int, val y: Int)
+data class MyXyPosition(override var x: Number, override var y: Number): XYPosition
+interface ReactFlowElement
+
 data class Labels(val label: String)
-data class MyElem(val id: String, val position: Coords, val data: Labels)
+data class MyNode(var id: String, var position: Any, var data: Any?): ReactFlowElement // :Node
+data class MyEdge(
+    override var id: ElementId,
+    override var source: ElementId,
+    override var target: ElementId,
+    override var animated: Boolean? = true
+): Edge, ReactFlowElement
 
 var elementsTo = arrayOf<Map<Any,Any>>(
     mapOf("id" to "1", "data" to mapOf("label" to  "node1"), "position" to mapOf("x" to 50, "y" to 5)),
     mapOf("id" to "2", "data" to mapOf("label" to "node2"), "position" to mapOf("x" to 100, "y" to 100)),
     mapOf("id" to "e1-2", "source" to "1", "target" to "2", "animated" to true)
     )
-
-//var elementsTo = <Map<Any,Any>>(
-//    mapOf("id" to "1", "data" to mapOf("label" to  "node1"), "position" to mapOf("x" to 50, "y" to 5)),
-//    mapOf("id" to "2", "data" to mapOf("label" to "node2"), "position" to mapOf("x" to 100, "y" to 100)),
-//    mapOf("id" to "e1-2", "source" to "1", "target" to "2", "animated" to true)
-//)
 fun main() {
-println("n1 $n1")
-
-
     render(document.getElementById("root")) {
         println("j $elementsTo ${elementsTo::class}")
         h1 {
@@ -52,9 +38,12 @@ println("n1 $n1")
             }
             ReactFlow {
                 attrs {
-                    elements = arrayOf(
-                        MyElem("22", Coords(100, 100), Labels("Node 1")),
-                        MyElem("23", Coords(50, 50), Labels("Node 2"))
+                    elements = arrayOf<ReactFlowElement>(
+                        MyNode("22", MyXyPosition(100, 100), Labels("Node 1")),
+                        MyNode("23", MyXyPosition(50, 50), Labels("Node 2"))
+//                        ,
+//                        MyEdge("1111", "22", "23")
+
                     )
                     styles = this@styledDiv.css {
                         height = 500.pt
