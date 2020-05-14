848cbdd2b2038dc07dd8930fdbd5a14f9d2474d1
jp <jp@skibeach.com>
2020-05-14 09:57:36 -0400

    edge connection works for new render

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,21 +6,19 @@ import styled.css
 import styled.styledDiv
 import kotlin.browser.document
 
-val n1 = Json.parseJson("""[{ "id": "1", "position":{"x":100, "y":100} }]""")  //, \"data\": { \"label\": \"Node 1\" }, position: {\"x\": 250, \"y\": 5 }
-
 data class MyXyPosition(override var x: Number, override var y: Number): XYPosition
 interface ReactFlowElement
 
 data class Labels(val label: String)
 data class MyNode(var id: String, var position: Any, var data: Any?): ReactFlowElement // :Node
 data class MyEdge(
-    override var id: ElementId,
-    override var source: ElementId,
-    override var target: ElementId,
-    override var animated: Boolean? = true
-): Edge, ReactFlowElement
+    var id: ElementId,
+    var source: ElementId,
+    var target: ElementId,
+    var animated: Boolean? = true
+): ReactFlowElement//, Edge
 
-var elementsTo = arrayOf<Map<Any,Any>>(
+    var elementsTo = arrayOf<Map<Any,Any>>(
     mapOf("id" to "1", "data" to mapOf("label" to  "node1"), "position" to mapOf("x" to 50, "y" to 5)),
     mapOf("id" to "2", "data" to mapOf("label" to "node2"), "position" to mapOf("x" to 100, "y" to 100)),
     mapOf("id" to "e1-2", "source" to "1", "target" to "2", "animated" to true)
@@ -40,9 +38,8 @@ fun main() {
                 attrs {
                     elements = arrayOf<ReactFlowElement>(
                         MyNode("22", MyXyPosition(100, 100), Labels("Node 1")),
-                        MyNode("23", MyXyPosition(50, 50), Labels("Node 2"))
-//                        ,
-//                        MyEdge("1111", "22", "23")
+                        MyNode("23", MyXyPosition(50, 50), Labels("Node 2")),
+                        MyEdge("1111", "22", "23")
 
                     )
                     styles = this@styledDiv.css {
